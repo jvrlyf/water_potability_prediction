e74eb08e8919ee4a029e2b4ca3678117ac59ea2a
jvrlyf <jvrinku@gmail.com>
2025-05-06 20:09:16 +0530

    Best Exp. dataset:   test_size: 0.2
    model_train:
      n_estimators: 300
      max_depth: 20
      min_samples_split: 5
      min_samples_leaf: 2

--- src/modeling/predict.py
+++ src/modeling/predict.py
@@ -7,6 +7,10 @@ import logging
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from typing import Tuple, Dict
 
+from dvclive import Live
+import yaml
+
+
 # Configure logging
 log_file_path = "predict.log"
 logging.basicConfig(
@@ -49,11 +53,31 @@ def load_model(filepath: str):
 
 def evaluate_model(model, X_test: pd.DataFrame, y_test: pd.Series) -> Dict[str, float]:
     try:
+        params=yaml.safe_load(open("params.yaml","r"))
+        test_size= params['dataset']['test_size']
+        n_estimators= params['model_train']['n_estimators']
+        max_depth= params['model_train']['max_depth']
+        min_samples_split= params['model_train']['min_samples_split']
+        min_samples_leaf= params['model_train']['min_samples_leaf']
+
         y_pred = model.predict(X_test)
         acc = accuracy_score(y_test, y_pred)
         pre = precision_score(y_test, y_pred)
         recall = recall_score(y_test, y_pred)
         f1score = f1_score(y_test, y_pred)
+
+        with Live(save_dvc_exp=True) as live:
+            live.log_metric("accuracy",acc)
+            live.log_metric("precision",pre)
+            live.log_metric("recall",recall)
+            live.log_metric("f1_score",f1score)
+
+            live.log_metric("test_size",test_size)
+            live.log_metric("n_estimators",n_estimators)
+            live.log_metric("max_depth",max_depth)
+            live.log_metric("min_samples_split",min_samples_split)
+            live.log_metric("min_samples_leaf",min_samples_leaf)
+
 
         metrics_dict = {
             "accuracy": acc,
